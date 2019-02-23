aa18892daf63768478088a8da3058f5290b095b2
Matthew Allbee <allbeematthew@gmail.com>
2019-02-23 13:26:34 -0600

    Created branch, added road data, and began work on RoadGraph.java

---
+++ Scenic_Trips-ejb/src/java/com/matthewallbee/ws/TestService.java
@@ -0,0 +1,8 @@
+@WebService
+public interfact TestService
+{
+	@WebMethod
+	public String test1();
+	@WebMethod
+	public String test2(String s);
+}
---
+++ Scenic_Trips-ejb/src/java/com/matthewallbee/ws/impl/TestServiceImpl.java
@@ -0,0 +1,16 @@
+import com.matthewallbee.ws.TestService;
+
+public class TestServiceImpl implements TestService
+{
+	@WebMethod
+	public String test1()
+	{
+		return "You have successfully called the test1 method!";
+	}
+
+	@WebMethod
+	public String test2(String inputStr)
+	{
+		return "You have passed \"" + inputStr + "\" into the test2 methodz";
+	}
+}
